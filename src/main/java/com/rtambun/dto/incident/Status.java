fb799c8ecdd96fd9d1703b3bb6249e1e4d0a7422
rtambun <richson.tambun@thalesgroup.com>
2022-06-08 13:41:03 +0800

    Add first test for Incident Dto

---
+++ src/main/java/com/rtambun/dto/incident/Status.java
@@ -0,0 +1,7 @@
+package com.rtambun.dto.incident;
+
+public enum Status {
+    OPEN,
+    ON_GOING,
+    CLOSED
+}
--- src/test/java/com/rtambun/dto/incident/IncidentTest.java
+++ src/test/java/com/rtambun/dto/incident/IncidentTest.java
@@ -2,6 +2,8 @@ package com.rtambun.dto.incident;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class IncidentTest {
@@ -13,6 +15,15 @@ class IncidentTest {
         assertThat(incident.getCloseDate()).isNull();
         assertThat(incident.getCreatedData()).isNull();
         assertThat(incident.getStatus()).isNull();
+
+        Instant now = Instant.now();
+        incident.setCreatedData(now);
+        incident.setCloseDate(now);
+        incident.setStatus(Status.OPEN);
+
+        assertThat(incident.getCloseDate()).isEqualTo(now);
+        assertThat(incident.getCreatedData()).isEqualTo(now);
+        assertThat(incident.getStatus()).isEqualTo(Status.OPEN);
     }
 
 }
